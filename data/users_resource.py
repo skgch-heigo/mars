b5a803958414e2f9d32694c554ec67d568733f5c
west1 <mstal64@yandex.ru>
2023-03-17 17:40:13 +0400

    api with users

--- data/users_resource.py
+++ data/users_resource.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from flask_restful import reqparse, abort, Api, Resource
 from data import db_session
 from data.beta_code import Jobs, User
-from data.user_parser import parser
+from data.user_parser import parser, parser2
 
 
 class UsersResource(Resource):
@@ -20,6 +20,28 @@ class UsersResource(Resource):
         session.delete(user)
         session.commit()
         return jsonify({'success': 'OK'})
+
+    def put(self, user_id):
+        abort_if_news_not_found(user_id)
+        args = parser2.parse_args()
+        session = db_session.create_session()
+        user = session.query(User).get(user_id)
+        if args['surname']:
+            user.surname = args['surname']
+        if args['name']:
+            user.name = args['name']
+        if args['age']:
+            user.age = args['age']
+        if args['position']:
+            user.position = args['position']
+        if args['speciality']:
+            user.speciality = args['speciality']
+        if args['address']:
+            user.address = args['address']
+        if args['email']:
+            user.email = args['email']
+        if args['hashed_password']:
+            user.hashed_password = args['hashed_password']
 
 
 class UsersListResource(Resource):
@@ -66,6 +88,32 @@ class UsersResourceDemo(Resource):
         session = db_session.create_session()
         user = session.query(User).get(user_id)
         session.delete(user)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+    def put(self, user_id):
+        abort_if_news_not_found(user_id)
+        args = parser2.parse_args()
+        session = db_session.create_session()
+        user = session.query(User).get(user_id)
+        if args['surname']:
+            user.surname = args['surname']
+        if args['name']:
+            user.name = args['name']
+        if args['age']:
+            user.age = args['age']
+        if args['position']:
+            user.position = args['position']
+        if args['speciality']:
+            user.speciality = args['speciality']
+        if args['address']:
+            user.address = args['address']
+        if args['email']:
+            user.email = args['email']
+        if args['hashed_password']:
+            user.hashed_password = args['hashed_password']
+
+        session.add(user)
         session.commit()
         return jsonify({'success': 'OK'})
 
---
+++ test6.py
@@ -0,0 +1,26 @@
+from requests import put, get, post, delete
+
+print(get('http://127.0.0.1:8080/api/users').json())
+
+print(get('http://127.0.0.1:8080/api/users/2').json())
+
+print(get('http://127.0.0.1:8080/api/users/168').json())
+# нет такого id
+
+print(post('http://127.0.0.1:8080/api/users',
+           json={'surname': "Wilson"}).json())
+# недостаточно данных
+
+print(post('http://127.0.0.1:8080/api/users',
+           json={'surname': "Wilson", 'name': "Lawrence", 'age': 26, 'position': "Detective",
+                 'speciality': "Detective work", 'address': "Redcoat Street, 34", 'email': "law_w2@google.com",
+                 "hashed_password": "pbkdf2:sha256:260000$aOEnFf8I5YHrGR1l$b3bdfe8e608703631552d07" +
+                                    "1bdbab0f8f5665c224f1d4df295de0135b441eb3f"}).json())
+
+print(delete('http://127.0.0.1:8080/api/users/4').json())
+
+print(delete('http://127.0.0.1:8080/api/users/999').json())
+# нет такого id
+
+print(put('http://127.0.0.1:8080/api/users/3',
+          json={'age': 27}).json())
